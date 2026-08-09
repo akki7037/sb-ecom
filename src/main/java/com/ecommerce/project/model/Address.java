d57425f3988db38cf6cae64ed5cb8cab6a1a368c
Aakash Tyagi <akashtyagiboy9568@gmail.com>
2026-08-09 17:33:56 +0530

    Authentication and authorization using JWT and cookie has been implemented.

---
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -0,0 +1,53 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Table(name = "addressed")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long addressId;
+
+    @NotBlank
+    @Size(min = 6, message = "please provide at least 6 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 2, message = "please provide at least 2 characters")
+    private String city;
+    @NotBlank
+    @Size(min = 2, message = "please provide at least 2 characters")
+    private String state;
+    @NotBlank
+    @Size(min = 3, message = "please provide at least 3 characters")
+    private String country;
+    @NotBlank
+    @Size(min = 6, message = "please provide at least 6 characters")
+    private String pincode;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+    public Address(String street, String city, String state, String country, String pincode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.pincode = pincode;
+    }
+}
